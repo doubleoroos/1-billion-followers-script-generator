c66fafcb70e35b5febf2cf75ad9fe60a17f2334a
Double O Roos <75936918+doubleoroos@users.noreply.github.com>
2025-10-23 19:57:36 +0200

    fix: Add missing newline at end of InputPanel.tsx
    Ensures consistent file formatting by adding a trailing newline character to the InputPanel.tsx file. This is a minor but important housekeeping change for code quality.

--- components/InputPanel.tsx
+++ components/InputPanel.tsx
@@ -1,4 +1,5 @@
 
+
 import React from 'react';
 import { SparklesIcon } from './icons/SparklesIcon';
 import type { EmotionalArcIntensity, VisualStyle, NarrativeTone, RewriteTomorrowTheme } from '../types';
